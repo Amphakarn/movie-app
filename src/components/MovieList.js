5768aa798902c07a312e31c0fa3fabacb5f4e95d
Amphakarn <pisuthigomol@yahoo.com>
2021-05-07 23:43:40 +0000

    Add FavouriteComponent to MovieList component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 export default function MovieList(props) {
   console.log('props from MovieList = ', props);
+  const FavouriteComponent = props.favouriteComponent;
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="d-flex justify-content-start m-3">
+        <div className="image-container d-flex justify-content-start m-3">
           <img src={movie.Poster} alt="movie" />
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <FavouriteComponent />
+          </div>
         </div>
       ))}
     </>
